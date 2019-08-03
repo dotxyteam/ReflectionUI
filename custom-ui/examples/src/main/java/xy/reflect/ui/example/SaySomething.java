658577aeb3b3cb1b63b1485831598ee8171e2155
dotxyteam <dotxyteam@yahoo.fr>
2019-08-03 11:52:00 +0000

    new example: SaySomething.java

---
+++ custom-ui/examples/src/main/java/xy/reflect/ui/example/SaySomething.java
@@ -0,0 +1,58 @@
+package xy.reflect.ui.example;
+
+import java.io.IOException;
+
+import xy.reflect.ui.CustomizedUI;
+import xy.reflect.ui.control.swing.customizer.SwingCustomizer;
+import xy.reflect.ui.util.MoreSystemProperties;
+
+/**
+ * Login screen GUI generated with the XML declarative customizations.
+ * 
+ * @author olitank
+ *
+ */
+public class SaySomething {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Set the following system property to disable the design mode:\n-D"
+				+ MoreSystemProperties.HIDE_INFO_CUSTOMIZATIONS_TOOLS + "=true");
+
+		CustomizedUI reflectionUI = new CustomizedUI();
+		SwingCustomizer renderer = new SwingCustomizer(reflectionUI, "saySomething.icu");
+		renderer.openObjectFrame(new SaySomething());
+	}
+
+	private String title;
+	private String emailAddress;
+	private String message;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void send() {
+		throw new UnsupportedOperationException();
+	}
+
+}
