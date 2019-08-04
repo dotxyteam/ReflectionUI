99c9cc5a8fa46e97e71905a3f7e8769d8421c4bd
dotxyteam <dotxyteam@yahoo.fr>
2019-08-04 12:06:57 +0000

    new example: control screen

---
+++ custom-ui/examples/src/main/java/xy/reflect/ui/example/ControlScreen.java
@@ -0,0 +1,57 @@
+package xy.reflect.ui.example;
+
+import java.io.IOException;
+
+import xy.reflect.ui.CustomizedUI;
+import xy.reflect.ui.control.swing.customizer.SwingCustomizer;
+import xy.reflect.ui.util.MoreSystemProperties;
+
+/**
+ * Login screen GUI generated with the XML declarative customizations.
+ * 
+ * @author olitank
+ *
+ */
+public class ControlScreen {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Set the following system property to disable the design mode:\n-D"
+				+ MoreSystemProperties.HIDE_INFO_CUSTOMIZATIONS_TOOLS + "=true");
+
+		CustomizedUI reflectionUI = new CustomizedUI();
+		SwingCustomizer renderer = new SwingCustomizer(reflectionUI, "controlScreen.icu");
+		renderer.openObjectFrame(new ControlScreen());
+	}
+
+	public Configuration getConfiguration() {
+		return new Configuration();
+	}
+
+	public Execution getExecution() {
+		return new Execution();
+	}
+
+	public Logs getLogs() {
+		return new Logs();
+	}
+
+	public Statistics getStatistics() {
+		return new Statistics();
+	}
+
+	public class Configuration {
+
+	}
+
+	public class Execution {
+
+	}
+
+	public class Logs {
+
+	}
+
+	public class Statistics {
+
+	}
+}
