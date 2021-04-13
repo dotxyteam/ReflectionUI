c5c363d00dfedc96a16d8458fbeac14e9084ebf5
dotxyteam <dotxyteam@yahoo.fr>
2021-04-13 20:13:14 +0200

    example update

--- custom-ui/unpackaged-src/SimpleReflectionUITest.java
+++ custom-ui/unpackaged-src/SimpleReflectionUITest.java
@@ -1,19 +1,11 @@
 
+import java.util.HashMap;
+
 import xy.reflect.ui.control.swing.customizer.SwingCustomizer;
 
 public class SimpleReflectionUITest {
 
-	public static class Test {
-		public int i = 0;
-		public int i2 = 0;
-		public int i3 = 0;
-
-		public void add(int increment) {
-			i += increment;
-		}
-	}
-
 	public static void main(String[] args) {
-		SwingCustomizer.getDefault().openObjectFrame(new Test(), "test", null);
+		SwingCustomizer.getDefault().openObjectFrame(new HashMap<Integer, String>(), "test", null);
 	}
 }
