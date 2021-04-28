b76c7e2df674485a39da2c8cd328be9fcd933e04
dotxyteam <dotxyteam@yahoo.fr>
2021-04-28 20:22:14 +0200

    example update

--- custom-ui/unpackaged-src/SimpleReflectionUITest.java
+++ custom-ui/unpackaged-src/SimpleReflectionUITest.java
@@ -1,9 +1,11 @@
 
 import xy.reflect.ui.control.swing.customizer.SwingCustomizer;
+import xy.reflect.ui.util.MoreSystemProperties;
 
 public class SimpleReflectionUITest {
 
 	public static void main(String[] args) {
-		SwingCustomizer.getDefault().openObjectFrame("azerty", "test", null);
+		System.setProperty(MoreSystemProperties.DEFAULT_INFO_CUSTOMIZATIONS_FILE_PATH, "unpackaged-src/simple.icu");
+		SwingCustomizer.getDefault().openObjectFrame("123", "test", null);
 	}
 }
