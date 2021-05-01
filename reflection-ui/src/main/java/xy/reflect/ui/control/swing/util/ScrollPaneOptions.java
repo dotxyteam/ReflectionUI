dcb493bae91145fe801fbab41abc69e29f60dd71
dotxyteam <dotxyteam@yahoo.fr>
2021-05-02 00:55:43 +0200

    form width limited by default

--- reflection-ui/src/main/java/xy/reflect/ui/control/swing/util/ScrollPaneOptions.java
+++ reflection-ui/src/main/java/xy/reflect/ui/control/swing/util/ScrollPaneOptions.java
@@ -55,22 +55,27 @@ public class ScrollPaneOptions extends ControlPanel implements Scrollable {
 		this.limitHeightToViewport = limitHeightToViewport;
 	}
 
+	@Override
 	public Dimension getPreferredScrollableViewportSize() {
 		return getPreferredSize();
 	}
 
+	@Override
 	public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
 		return 10;
 	}
 
+	@Override
 	public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
 		return ((orientation == SwingConstants.VERTICAL) ? visibleRect.height : visibleRect.width) - 10;
 	}
 
+	@Override
 	public boolean getScrollableTracksViewportWidth() {
 		return limitWidthToViewport;
 	}
 
+	@Override
 	public boolean getScrollableTracksViewportHeight() {
 		return limitHeightToViewport;
 	}
