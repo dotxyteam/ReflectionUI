8e7019a5c39ee38e7d9d28910b13ef6d3a8bcc72
dotxyteam <dotxyteam@yahoo.fr>
2023-06-04 14:31:32 +0200

    examples fix

--- custom-ui/examples/src/main/java/xy/reflect/ui/example/CurrencyConverter.java
+++ custom-ui/examples/src/main/java/xy/reflect/ui/example/CurrencyConverter.java
@@ -72,7 +72,7 @@ public class CurrencyConverter {
 
 	private double getSourceCurrencyAmountValue() throws ScriptException {
 		ScriptEngineManager factory = new ScriptEngineManager();
-		ScriptEngine engine = factory.getEngineByName("JavaScript");
+		ScriptEngine engine = factory.getEngineByName("graal.js");
 		return ((Number) engine.eval(sourceCurrencyAmount)).doubleValue();
 	}
 
