f01e7fd08d5a0aedc9b8742c4c0963a1089030c4
dotxyteam <dotxyteam@yahoo.fr>
2025-05-02 09:15:52 +0200

    debugging type cache issues

--- reflection-ui/src/main/java/xy/reflect/ui/ReflectionUI.java
+++ reflection-ui/src/main/java/xy/reflect/ui/ReflectionUI.java
@@ -32,7 +32,8 @@ public class ReflectionUI {
 
 	protected static ReflectionUI defaultInstance;
 
-	protected Map<Object, ITypeInfo> typeCache = MiscUtils.newWeakValuesEqualityBasedMap();
+	protected Map<Object, ITypeInfo> typeCache = MiscUtils.newAutoCleanUpCache(false, false, 1000, 5000,
+			"TypeCacheCleaner[of=" + this + "]");
 	protected Object typeCacheMutex = new Object();
 
 	/**
